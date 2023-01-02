18b1bae65711291b57c3113dd21a9e3f096f32a6
enoshima7 <duanjipeng0328@gmail.com>
2023-01-02 22:52:18 +0800

    shuffle

--- src/logics/index.ts
+++ src/logics/index.ts
@@ -8,3 +8,22 @@ export function formatDate(d: string | Date) {
     return date.format('MMM D')
   return date.format('MMM D, YYYY')
 }
+
+// https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+export function shuffle(array: any[]) {
+  let currentIndex = array.length
+  let randomIndex
+
+  // While there remain elements to shuffle.
+  while (currentIndex !== 0) {
+    // Pick a remaining element.
+    randomIndex = Math.floor(Math.random() * currentIndex)
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex], array[currentIndex]]
+  }
+
+  return array
+}
