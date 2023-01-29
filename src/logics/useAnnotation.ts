7353152ac9dab409dfd3d7ed5017d1aa90375bb8
enoshima7 <duanjipeng0328@gmail.com>
2023-01-29 23:12:58 +0800

    fix

--- src/logics/useAnnotation.ts
+++ src/logics/useAnnotation.ts
@@ -1,3 +1,5 @@
+import { useFetch } from '@vueuse/core'
+
 export function useAnnotation(str: string) {
   return useFetch(`${import.meta.env.VITE_APP_API_URL}/api/ruby?text=${str}&mode=furigana&to=hiragana`, {
     immediate: true,
